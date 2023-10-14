7bdb9eaf2e82914f5b8ef55681af4a619e948294
Nazmul Alam <shawonkst15@gmail.com>
2023-10-14 20:32:27 +0600

    arrow function

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ const appleOrangeJuice = fruitProcessor(5, 4);
 console.log(appleOrangeJuice);
 const num = Number('23');
 console.log(num);
+//  function declaration
+function calcAge1(birthYear) {
+	return 2037 - birthYear;
+}
+const age1 = calcAge1(1998);
+console.log(age1);
+
+// anonymouse function and function expression
+const calcAge2 = function (birthYear) {
+	return 2037 - birthYear;
+};
+const age2 = calcAge2(1989);
+console.log(age1, age2);
